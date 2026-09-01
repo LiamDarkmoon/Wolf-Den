d7d946420d041fcab3de498a828245bed93418e4
Liam <liamdarkmoonm@gmail.com>
2026-09-01 16:40:06 -0300

    feat: enhance CharacterSelector component layout and add user role validation in middleware

--- src/components/CharacterSelector.tsx
+++ src/components/CharacterSelector.tsx
@@ -95,42 +95,54 @@ export default function CharacterSelector({
 
     return (
         <>
-            <li className="flex items-center italic my-2">
+            <li className="flex items-center italic my-3">
 
-                {/* Selector solamente para el usuario actual */}
-                {isOwner && (
+                {isOwner ? (
                     <button
-                        className="size-8 text-primary cursor-pointer me-1 hover:text-primary-hover"
+                        className="size-8 grid place-items-center text-primary cursor-pointer me-1 hover:text-primary-hover"
                         onClick={handleDropdown}
                     >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="size-6"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M3.75 6.75h16.5M3.75 12H12m-8.25 5.25h16.5"
-                            />
+                        <svg 
+                            xmlns="http://www.w3.org/2000/svg" 
+                            fill="none" viewBox="0 0 24 24" 
+                            strokeWidth={1.5} stroke="currentColor" 
+                            className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                         </svg>
+
                     </button>
-                )}
+                ) :
+                    <button
+                        className="size-8 grid place-items-center text-secondary cursor-pointer me-1"
+                    >
+                        <svg 
+                            xmlns="http://www.w3.org/2000/svg" 
+                            fill="none" 
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5} 
+                            stroke="currentColor" 
+                            className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
+                        </svg>
 
-                {player.display_name}
+                    </button>
+                }
+                
+                <div className="flex flex-col">
 
-                <span className="text-xs text-secondary-text opacity-50">
-                    ({player.user_id.slice(0, 6)})
-                </span>
+                    <span className="flex items-center">
+                    {player.display_name}
+                        <span className="text-xs opacity-50">
+                            ({player.user_id.slice(0, 6)})
+                        </span>:
+                    </span>
 
-                {" → "}
+                    <span className="">
+                    Personaje: -&gt; {selectedCharacter ?? "Sin personaje"}
+                    </span>
 
-                <span className="text-secondary ms-2">
-                    {selectedCharacter ?? "Sin personaje"}
-                </span>
+                </div>
+
 
             </li>
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -21,15 +21,39 @@ export const onRequest = defineMiddleware(
 
     const {
       data: { user },
-      error,
     } = await supabase.auth.getUser();
-
-    
 
     locals.user = user;
 
+    // Si no hay usuario y la ruta es privada
     if (!user && !isPublicRoute) {
       return redirect("/auth/login");
+    }
+
+    // Si intenta acceder al admin
+    if (url.pathname.startsWith("/admin")) {
+
+      if (!user) {
+        return redirect("/auth/login");
+      }
+
+      const { data: profile, error } = await supabase
+        .from("profiles")
+        .select("role")
+        .eq("id", user.id)
+        .single();
+
+      locals.userRole = profile?.role ?? null;
+
+      if (error || !profile) {
+        return redirect("/");
+      }
+
+      const userRole = profile.role;
+
+      if (!["admin", "super_admin"].includes(userRole)) {
+        return redirect("/");
+      }
     }
 
     return next();
